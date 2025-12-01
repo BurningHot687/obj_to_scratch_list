5557a3594b4fc7da91f3dba8b54ef84ae1b8c8ef
BurningHot687 <222676217+BurningHot687@users.noreply.github.com>
2025-11-30 18:46:47 -0600

    Error Handling, but broke txt handling

--- src/obj_to_scratch_list.py
+++ src/obj_to_scratch_list.py
@@ -9,22 +9,26 @@ def make_scratch_list():
     open(txt_dir, 'w').close() # Create the file if it does not exist
 
 # File path variables
-obj_name = input("Name of obj file (without extension): ") # Input your .obj file name here without extension
-obj_read = os.path.join(os.path.expanduser("~"), input("File path of obj from home/absolute directory: "), f"{obj_name}.obj")
-txt_write = f"{input("Name of txt file to look/create: ")}.txt" # Output .txt file name, change if approaching 200,000 lines
+obj_read = ""
+while not os.path.exists(obj_read):
+    obj_name = f"{input("Name of obj file (without extension): ")}.obj" # Input your .obj file name here without extension
+    obj_read = input("File path of obj from home/absolute directory: ")
+    obj_read = os.path.join(os.path.expanduser("~"), obj_read, obj_name)
+txt_write = f"{input("Name of txt file to look/create (without extension): ")}.txt" # Output .txt file name, change if approaching 200,000 lines
 txt_dir = os.path.join(os.path.expanduser("~"), input("File path to save scratch list (from home/absolute directory): "), txt_write)
 
 # Handle the wonkiness of the output file path lol
 if not os.path.exists(txt_dir):
     make_scratch_list()
 else:
-    append_delete = input(f"The file {txt_dir} already exists. Type 'a' to append or 'd' to delete and create a new file: ").lower() # Ask to append or delete file
+    print(f"The file {txt_dir} already exists.")
+    append_delete = input("Press anything (like enter) to append or 'd' to delete and create a new file: ").lower() # Ask to append or delete file
     if append_delete == 'd':
         try:
             os.remove(txt_dir) # Delete the file
         except FileNotFoundError:
             print("An unexpected error occurred while trying to delete the file. A new file is being created.")
-            make_scratch_list()
+        make_scratch_list()
 
 # Model Variables
 num_colors = 0 # Number of colors used in the model, set to 0 for default color
@@ -42,7 +46,7 @@ with open(obj_read, "r") as obj_file:
         if line.startswith("f "):
             obj_lines += 1 # Count number of faces for estimation
     try:
-        with open(txt_write, "r") as txt_file:
+        with open(txt_dir, "r") as txt_file:
             for line in txt_file:
                 txt_lines += 1 # Count number of lines
     except FileNotFoundError:
@@ -51,7 +55,7 @@ with open(obj_read, "r") as obj_file:
     print("List will fit the new scratch list file." if txt_lines + obj_lines < 200000 else "List will NOT fit the new scratch list file.")
     sys.exit(0) if txt_lines + obj_lines > 200000 else None
 
-with open(obj_read, "r") as obj_file, open(txt_write, "a") as txt_file:
+with open(obj_read, "r") as obj_file, open(txt_dir, "a") as txt_file:
     txt_file.write(obj_name + "\n") # Write the model name as the first line
     txt_file.write(f"{num_colors}\n") # Write number of colors used
     for line in obj_file:
@@ -68,4 +72,4 @@ with open(obj_read, "r") as obj_file, open(txt_write, "a") as txt_file:
         for index in face:
             current_line = f"{current_line}{vertices[index-1][0]} {vertices[index-1][1]} {vertices[index-1][2]} "
         txt_file.write(current_line + "\n") # Write the face vertices to the txt file 
-os.startfile(txt_write)
+os.startfile(txt_dir)
